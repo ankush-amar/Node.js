4611a8ac81d1331c02a8d11d52ce4ec9f5976db5
Ankush Amar <ankushamar018@gmail.com>
2026-08-17 18:54:34 +0530

    17-aug-26

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.headers, req.method);
+    
     if(req.url === '/'){
-        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Content-Type' , 'text/html');
         res.write('<html>');
         res.write('<head><title>Ankush Amar</title></head>');
-        res.write('<body><h1>Welcome Home!</h1></body>');
+        res.write('<body><h1>Enter your details</h1></body>');
+
+        res.write('<form action="/submit-details" method="POST">');
+        res.write('<input type="text" name="username" placeholer="Enter your name"><br>');
+        res.write('<label for="male">Male</label>');
+        res.write('<input type="radio" id="male" name="gender" value="male">');
+        res.write('<label for="female">female</label>');
+        res.write('<input type="radio" id="female" name="gender" value="female">');
+        res.write('<br><input type="submit" value="Submit"></br>');
+        res.write('</form>');
+
+        res.write('</body>');
         res.write('</html>');
         return res.end();
-    }else if(req.url === '/products'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Ankush Amar</title></head>');
-        res.write('<body><h1>Check out some products</h1></body>');
-        res.write('</html>');
-        return res.end();
-    }else if(req.url === '/mens'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Ankush Amar</title></head>');
-        res.write('<body><h1>These are some mens wear</h1></body>');
-        res.write('</html>');
-        return res.end();
-    }else if(req.url === '/womens'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Ankush Amar</title></head>');
-        res.write('<body><h1>These are some womens wear</h1></body>');
-        res.write('</html>');
-        return res.end();
+
+    }else if(req.url.toLowerCase() === '/submit-details' && req.method == 'POST' ){
+        fs.writeFileSync('user.txt', 'Ankush Amar');
+        res.statusCode = 302;
+        res.setHeader('Location' , '/');
+
     }
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type' , 'text/html');
     res.write('<html>');
     res.write('<head><title>Ankush Amar</title></head>');
     res.write('<body><h1>Hello I am Ankush Amar</h1></body>');
-    res.write('</html>');
+    res.write('</html>')
     res.end();
 });
 
